bc617ac6eb62a1d655afc0670423b7d441a824b4
Rui <rui@dhcp88.merusouth120.iit.edu>
2018-01-26 18:30:24 -0600

    first commig

---
+++ src/Solution.java
@@ -0,0 +1,36 @@
+
+//Definition for binary tree with next pointer.
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) { val = x; }
+}
+
+
+public class Solution {
+	public void connect(TreeLinkNode root) {
+        if(root == null)
+        	return;
+        TreeLinkNode p = root.next;
+        while(p != null){
+        	if(p.left != null){
+        		p = p.left;
+        		break;
+        	}
+        	if(p.right != null){
+        		p = p.right;
+        		break;
+        	}
+        	p = p.next;
+        }
+        
+        if(root.left != null)
+        	root.left.next = root.right == null ? p : root.right;
+        
+        if(root.right != null)
+        	root.right.next = p;
+        
+        connect(root.left);
+        connect(root.right);
+    }
+}
